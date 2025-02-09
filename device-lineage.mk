fe9b45d51919287ba7132878d34beb0304a0efd1
IczYn <iczyn.iczyn@gmail.com>
2025-02-09 17:01:34 +0000

    bluejay: Use SCUDO
    Signed-off-by: IczYn <iczyn.iczyn@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,6 +6,10 @@
 
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
 
+# Use 32-bit libc variant by default
+PRODUCT_USE_SCUDO := true
+PRODUCT_USE_SCUDO_32_BIT := true
+
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
     ANGLE
